d89d0151ca39ee4074310b913edc58e10a830a31
ThatFrogDev <mart.zielman@kpnmail.nl>
2024-08-19 21:25:10 +0200

    feat: temporary ban

--- src/commands/moderation/ban.ts
+++ src/commands/moderation/ban.ts
@@ -4,6 +4,9 @@ import {
   type ChatInputCommandInteraction
 } from "discord.js";
 import { errorCheck, modEmbed } from "../../utils/embeds/modEmbed";
+import { errorEmbed } from "../../utils/embeds/errorEmbed";
+import Unban from "./unban";
+import ms from "ms";
 
 export default class Ban {
   data: SlashCommandSubcommandBuilder;
@@ -24,6 +27,21 @@ export default class Ban {
 
   async run(interaction: ChatInputCommandInteraction) {
     const user = interaction.options.getUser("user")!;
+    const guild = interaction.guild!;
+    const duration = interaction.options.getString("duration", false)!;
+
+    if (duration) {
+      if (!ms(duration))
+        return await errorEmbed(
+          interaction,
+          `You can't ban ${user.displayName} temporary.`,
+          "The duration is invalid"
+        );
+
+      setTimeout(() => {
+        await guild.members.unban(user.id, reason ?? undefined).catch(error => console.error(error));
+      }, ms(duration));
+    }
 
     await errorCheck(
       PermissionsBitField.Flags.BanMembers,
@@ -38,6 +56,8 @@ export default class Ban {
       ?.ban({ reason: reason ?? undefined })
       .catch(error => console.error(error));
 
-    await modEmbed({ interaction, user, action: "Banned" }, reason);
+    await modEmbed({ interaction, user, action: "Banned", duration? }, reason );
+    // TODO: Send the user a message when they're banned including the duration
+    }
   }
 }
