d46e1fddfeda992d394efd4569249b2c3916384a
iakzs <k@iakzs.is-a.dev>
2024-09-15 10:35:17 -0300

    added the expired ban check at the start

--- src/bot.ts
+++ src/bot.ts
@@ -1,6 +1,7 @@
 import { ActivityType, Client } from "discord.js";
 import { Commands } from "./handlers/commands";
 import { Events } from "./handlers/events";
+import { getExpiredBans, removeModeration } from "./utils/database/moderation";
 
 const client = new Client({
   presence: {
@@ -17,10 +18,41 @@ const client = new Client({
   ]
 });
 
+const checkInterval = 60 * 500;
+
 client.on("ready", async () => {
   await new Events(client).loadEvents();
   await new Commands(client).registerCommands();
   console.log("ちーっす！");
+  setInterval(async () => {
+    const now = Date.now();
+    const expiredBans = getExpiredBans(now);
+
+    for (const ban of expiredBans) {
+      try {
+        const guild = await client.guilds.fetch(ban.guild).catch(() => null);
+        if (!guild) {
+          removeModeration(ban.guild, ban.id);
+          continue;
+        }
+
+        const userId = ban.user.toString();
+
+        const banInfo = await guild.bans.fetch(userId).catch(() => null);
+        if (!banInfo) {
+          removeModeration(ban.guild, ban.id);
+          continue;
+        }
+
+        await guild.members.unban(userId, "Temporary ban expired");
+
+        removeModeration(ban.guild, ban.id);
+
+      } catch (error) {
+        console.error(`Failed to unban user ID ${ban.user} in guild ${ban.guild}:`, error);
+      }
+    }
+  }, checkInterval);
 });
 
 client.login(process.env.TOKEN);
