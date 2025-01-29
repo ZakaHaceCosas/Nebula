a71ef2adec4e7752a1b4792dad7f8a9fa775d774
Zaka <zakahacecosas@protonmail.com>
2025-01-29 20:42:36 +0100

    Humanize settings display + capitalize() don't return undefined

--- src/commands/settings.ts
+++ src/commands/settings.ts
@@ -16,6 +16,7 @@ import {
 } from "../utils/database/settings";
 import { errorEmbed } from "../utils/embeds/errorEmbed";
 import { capitalize } from "../utils/capitalize";
+import { humanizeSettings } from "../utils/humanizeSettings";
 
 export let data = new SlashCommandBuilder()
   .setName("settings")
@@ -94,7 +95,7 @@ export async function run(interaction: ChatInputCommandInteraction) {
   const key = interaction.options.getSubcommand() as keyof typeof settingsDefinition;
   const values = interaction.options.data[0].options!;
   const settingsDef = settingsDefinition[key];
-  const settingText = (name: string) => {
+  const settingText = (name: string): string => {
     const setting = getSetting(guild.id, key, name)?.toString();
     let text;
     switch (settingsDef.settings[name].type) {
@@ -108,7 +109,7 @@ export async function run(interaction: ChatInputCommandInteraction) {
         text = setting ? `<@&${setting}>` : "Not set";
         break;
       default:
-        text = setting || "Not set";
+        text = setting || "*Not set*";
         break;
     }
     return text;
@@ -118,7 +119,7 @@ export async function run(interaction: ChatInputCommandInteraction) {
     const embed = new EmbedBuilder()
       .setAuthor({ name: `${capitalize(key)} settings` })
       .setDescription(
-        Object.keys(settingsDef.settings).map(setting => `${settingsDef.settings[setting].emoji} **• ${capitalize(setting.replaceAll("_", " "))}**: ${settingText(setting)}`).join("\n")
+        Object.keys(settingsDef.settings).map(setting => `${settingsDef.settings[setting].emoji} **• ${humanizeSettings(capitalize(setting))}**: ${humanizeSettings(settingText(setting))}`).join("\n")
       )
       .setColor(genColor(100));
 
@@ -147,7 +148,7 @@ export async function run(interaction: ChatInputCommandInteraction) {
       );
 
     setSetting(guild.id, key, option.name, option.value as string);
-    description += `**${capitalize(option.name)?.replace("_", " ")}:** ${settingText(option.name.toString()!)}\n`
+    description += `**${humanizeSettings(capitalize(option.name))}:** ${humanizeSettings(settingText(option.name.toString()))}\n`
   }
   embed.setDescription(description)
 
